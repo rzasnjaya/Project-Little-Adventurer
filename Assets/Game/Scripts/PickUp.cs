c8b505c192e534a1fd20affc8163b01d6be9b850
rzasnjaya <sanjayareza77@gmail.com>
2026-04-27 16:06:50 +0700

    Creating the gate

--- Assets/Game/Scripts/PickUp.cs
+++ Assets/Game/Scripts/PickUp.cs
@@ -11,12 +11,19 @@ public class PickUp : MonoBehaviour
 
     public PickUpType Type;
     public int Value = 20;
+    public ParticleSystem CollectedVFX;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             other.gameObject.GetComponent<Character>().PickUpItem(this);
+
+            if(CollectedVFX != null)
+            {
+                Instantiate(CollectedVFX, transform.position, Quaternion.identity);
+            }
+
             Destroy(gameObject);
         }
     }
